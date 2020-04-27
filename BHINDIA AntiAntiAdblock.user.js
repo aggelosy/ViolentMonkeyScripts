e7f6d38e094dfecc2e83895a065ab1d92abf3a1d
aggelosy <aggelosyian@aim.com>
2020-04-27 12:31:51 +0300

    Delete BHINDIA AntiAntiAdblock.user.js

--- BHINDIA AntiAntiAdblock.user.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name BHINDIA AntiAntiAdblock
-// @namespace Violentmonkey Scripts
-// @include https://forum.blackhatindia.ru/*
-// @grant none
-// ==/UserScript==
-
-document.getElementById("blogantiadblockbox").remove();
-document.getElementById("g207").remove();
